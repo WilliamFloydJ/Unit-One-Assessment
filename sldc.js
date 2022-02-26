13ff98d361c8105e730e951295980318e4519a57
William <Floydfamily71@gmail.com>
2022-02-25 19:08:47 -0700

    Working Iteration

--- sldc.js
+++ sldc.js
@@ -4,13 +4,13 @@
 /*
     Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
 */
-
+// Planning
 // Analysis of Requirements
 // Design
 // Implementation
-// Maintain
-// Planning
 // Testing & Integration
+// Maintain
+
 
 
 //////////////////PROBLEM 2////////////////////
@@ -19,3 +19,14 @@
     The first one is done for you. 
 */
 const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+const Analysis = "This is the second step, in which you analize the required objectives and reaserch the needs of the user."
+const Design = "This is the third step, this is where you create a layout for the spacific details in the application. Some of which are Interfaces and databases."
+const Implementation = "This is the fourth step, in which you actually write the code for the application. This is when you reference the previous steps to implement the ideas and reaserch you and your team have had."
+const TestingIntegration = "This is the fifth step, this is where after you've created the software you now have to test and refine for bugs and anything that might be hindering the program. This step also includes the proccess of integrating design elements."
+const Maintain = "This is the sixth and final step, in which you make sure the software runs efficiantly and preforms appropriatly after you launch. Developers will need to handle any issues that come up from either user input or bugs not caught in the testing fase."
+
+
+
+
+
+
